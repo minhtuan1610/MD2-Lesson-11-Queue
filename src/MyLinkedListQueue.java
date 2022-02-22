d95cfb7c26fcef3a62252a516de6d956bf9997da
Nguyễn Minh Tuấn <tuannm1610@gmail.com>
2022-02-23 00:27:09 +0700

    trien khai Queue su dung danh sach lien ket

---
+++ src/MyLinkedListQueue.java
@@ -0,0 +1,33 @@
+public class MyLinkedListQueue {
+    private Node head;
+    private Node tail;
+
+    public MyLinkedListQueue() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    //  Cai dat phuong thuc enqueue trong lop MyLinkedListQueue
+    public void enqueue(int key) {
+        Node temp = new Node(key);
+        if (this.tail == null) {
+            this.head = this.tail = temp;
+            return;
+        }
+        this.tail.next = temp;
+        this.tail = temp;
+    }
+
+    //  Cai dat phuong thuc dequeue trong lop MyLinkedListQueue
+    public Node dequeue() {
+        if (this.head == null) {
+            return null;
+        }
+        Node temp = this.head;
+        this.head = this.head.next;
+        if (this.head == null) {
+            this.tail = null;
+        }
+        return temp;
+    }
+}
